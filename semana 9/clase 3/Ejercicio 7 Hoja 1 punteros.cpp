4580c7d673c1e341da6cc19a5d2616ab2bd50397
tvwc3rl3 <50773855+tvwc3rl3@users.noreply.github.com>
2019-05-20 09:16:29 -0500

    Create Ejercicio 7 Hoja 1 punteros.cpp

---
+++ semana 9/clase 3/Ejercicio 7 Hoja 1 punteros.cpp
@@ -0,0 +1,44 @@
+// Hoja 1 punteros Ejercicio 7
+
+#include "stdafx.h"
+#include <conio.h>
+#include <iostream>
+#include <Windows.h>
+
+using namespace System;
+using namespace std;
+
+int HallarNumeroInvertido(int *numero) {
+	int *digito = new int;
+	int *nuevoNumero = new int;
+
+	*nuevoNumero = 0;
+	while (*numero > 0)
+	{
+		*digito = *numero % 10;
+		*nuevoNumero = *nuevoNumero * 10 + *digito;
+		*numero = *numero / 10;
+	}
+	return *nuevoNumero;
+}
+
+int main()
+{
+	//El tipo de dato int puede tener hasta 10 cifras
+	//El tipo de dato long long puede tenner hasta 19 cifras
+	int *numero = new int;
+	int *numeroInvertido = new int;
+	int *cantCifras = new int;
+
+	while (1)
+	{
+		cout << "Ingrese un numero: ";
+		cin >> *numero;
+		if (*numero > 999)
+			break;
+	}
+	*numeroInvertido = HallarNumeroInvertido(numero);
+	cout << "El numero de cifras es: " << *numeroInvertido;
+	_getch();
+	return 0;
+}
