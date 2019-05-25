2b617508b8aab797a8569005ffee5c0a821db86b
tvwc3rl3 <50773855+tvwc3rl3@users.noreply.github.com>
2019-05-25 10:11:33 -0500

    Update asd.cpp

--- semana 10/clase 2/asd.cpp
+++ semana 10/clase 2/asd.cpp
@@ -5,18 +5,7 @@
 using namespace System;
 using namespace std;
 
-
-
-int main()
-{
-
-	char *opcion = new char;
-	int *elfactorial = new int;
-	int *numero = new int;
-	int *k = new int;
-	int *numRombo = new int;
-	float *elexponencial = new float;
-	float *a = new float;
+void Menu(char *opcion) {
 	
 	cout << "\t\t\t" << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << endl;
 	cout << "\t\t\t" "MENU PRINCIPAL" << endl;
@@ -25,32 +14,33 @@ int main()
 	cout << "\t\tB. Restar 2 Números Complejos." << endl;
 	cout << "\t\tX. Salir del Programa" << endl;
 	
-	switch (switch_on)
+	switch (*opcion)
 	{
-	case 1:
+	case 'A':
 		break;
-	case 2:
+	case 'B':
 		break;
-	default:
+	case 'X':
 		break;
 	}
-	
+}
+
+
+
+int main()
+{
+
+	char *opcion = new char;
+
+		
+	Menu(opcion);
+	cout << "Ingrese la opcion";
+	cin >> *opcion;
+
 	//liberamos memoria
 	delete opcion;
-	delete elfactorial;
-	delete numero;
-	delete k;
-	delete numRombo;
-	delete elexponencial;
-	delete a;
 	opcion = NULL;
-	elfactorial = NULL;
-	numero = NULL;
-	k = NULL;
-	numRombo = NULL;
-	elexponencial = NULL;
-	a = NULL;
-
+	
 	_getch();
 	return 0;
 }
