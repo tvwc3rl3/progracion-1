3b6c1232a5d44e81e7cccf93aef2ddc098cd9534
tvwc3rl3 <50773855+tvwc3rl3@users.noreply.github.com>
2019-06-03 10:50:30 -0500

    Create ejercicio.cpp

---
+++ semana 12/clase 1/ejercicio.cpp
@@ -0,0 +1,35 @@
+// ConsoleApplication4.cpp : main project file.
+
+#include "stdafx.h"
+#include <iostream>
+#include <conio.h>
+#include <time.h>
+
+using namespace System;
+using namespace std;
+
+//La estructura tiene como alias "Persona" y tiene 2 miembros: edad y sexo
+
+typedef struct {
+	int edad;
+	char sexo;
+} Persona;
+
+int main()
+{
+	//creamos un arreglo de la estructura persona
+	Persona *arrPersona;
+	arrPersona = new Persona[5];
+
+	int *indice = new int;
+	//Ingresar los datos a cada elemnto del arreglo
+	for (*indice = 0; *indice < 5; (*indice)++)
+	{
+		cout << "Ingrese la edad del elemento " << *indice + 1 << " : ";
+		cin >> arrPersona[*indice].edad;
+		cout << "Ingrese el sexo: " << *indice + 1 << " : ";
+		cin >> arrPersona[*indice].sexo;
+	}
+    _getch();
+    return 0;
+}
