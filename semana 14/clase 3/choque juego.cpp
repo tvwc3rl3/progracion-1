7302ad0cc8dabcf91a659bbf9176f59b3e6f0fd6
tvwc3rl3 <50773855+tvwc3rl3@users.noreply.github.com>
2019-06-28 14:07:48 -0500

    Update choque juego.cpp

--- semana 14/clase 3/choque juego.cpp
+++ semana 14/clase 3/choque juego.cpp
@@ -14,21 +14,34 @@ const int ABAJO = 80;
 const int DERECHA = 77;
 const int IZQUIERDA = 75;
 
+void borraMovil(int *posMovil) {
+	Console::SetCursorPosition(posMovil[0], posMovil[1]);
+	Console::ForegroundColor = ConsoleColor::Cyan;
+	cout << char(219);
+}
+
 void dibujaMovil(int *posMovil)
 {
-	Console::SetCursorPosition(posMovil[0],posMovil[1]);
+	Console::SetCursorPosition(posMovil[0], posMovil[1]);
 	Console::ForegroundColor = ConsoleColor::Yellow;
 	cout << char(3);
+}
+
+void dibujaEnemigo(int *posEnemigo)
+{
+	Console::SetCursorPosition(posEnemigo[0], posEnemigo[1]);
+	Console::ForegroundColor = ConsoleColor::Magenta;
+	cout << char(1);
 }
 
 void borraEnemigo(int *posEnemigo)
 {
 	Console::SetCursorPosition(posEnemigo[0], posEnemigo[1]);
 	Console::ForegroundColor = ConsoleColor::Cyan;
-	cout << " ";
+	cout << char(219);
 }
 
-void moverEnemigo(int **mapa, int *posEnemigo, int avance, int *posMovil)
+void moverEnemigo(int **mapa, int *posEnemigo, int &avance, int *posMovil)
 {
 	//colisiona con el enemigo
 	if (posEnemigo[0] == posMovil[0] && posEnemigo[1] == posMovil[1])
@@ -49,31 +62,85 @@ void moverEnemigo(int **mapa, int *posEnemigo, int avance, int *posMovil)
 }
 
 
-void dibujaEnemigo(int *posEnemigo)
-{
-	Console::SetCursorPosition(posEnemigo[0], posEnemigo[1]);
-	Console::ForegroundColor = ConsoleColor::Magenta;
-	cout << char(1);
-}
 
 
 void moverPersonaje(int **mapa, int *posMovil, char tecla)
-{}
+{
+	borraMovil(posMovil);
+	//mover
+	if (tecla == DERECHA) {
+		if (mapa[posMovil[1]][posMovil[0]+1]==1)
+		{
+			posMovil[0] = posMovil[0] + 1;
+		}
+		else
+		{
+			posMovil[0] = 10;
+			posMovil[1] = 8;
+		}
+	}
+	else 
+	{
+		if (tecla == IZQUIERDA) {
+			if (mapa[posMovil[1]][posMovil[0] - 1] == 1)
+			{
+				posMovil[0] = posMovil[0] - 1;
+			}
+			else
+			{
+				posMovil[0] = 10;
+				posMovil[1] = 8;
+			}
+		}
+		else
+		{
+			if (tecla == ARRIBA) {
+				if (mapa[posMovil[1]-1][posMovil[0]] == 1)
+				{
+					posMovil[1] = posMovil[1] - 1;
+				}
+				else
+				{
+					posMovil[0] = 10;
+					posMovil[1] = 8;
+				}
+			}
+			else
+			{
+				if (tecla == ABAJO) {
+					if (mapa[posMovil[1]+1][posMovil[0]] == 1)
+					{
+						posMovil[1] = posMovil[1] + 1;
+					}
+					else
+					{
+						posMovil[0] = 10;
+						posMovil[1] = 8;
+					}
+				}
+			}
+		}
+	}
+
+	//dibujar
+	dibujaMovil(posMovil);
+
+}
 
 void desplazar_movil_y_enemigo(int **mapa)
 {
 	int *posMovil, *posEnemigo, avance;
 	char tecla;
-	
+
 	posMovil = new int[2];
 	posEnemigo = new int[2];
 	//Posicion Inicial Movil
 	posMovil[0] = 10; //x;
 	posMovil[1] = 8; //y
-	//POsicion Inicial Enemigo
+					 //POsicion Inicial Enemigo
 	posEnemigo[0] = 19; //x
 	posEnemigo[1] = 8; //y
-	//dibujamos al movil
+					   //dibujamos al movil
 	dibujaMovil(posMovil);
 	//dibujamos al enemigo
 	dibujaEnemigo(posEnemigo);
@@ -97,27 +164,27 @@ void desplazar_movil_y_enemigo(int **mapa)
 
 void genera_muestra_matriz(int **mapa)
 {
-	int matriz [20][23]= { 
-	{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
-	{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
-	{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
-	{ 0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0 },
-	{ 0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0 },
-	{ 0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0 },
-	{ 0,0,0,0,0,1,0,0,0,0,1,1,1,0,0,0,0,0,0,1,0,0,0 },
-	{ 0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0 },
-	{ 0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0 },
-	{ 0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
-	{ 0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
-	{ 0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
-	{ 0,0,0,0,0,1,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0 },
-	{ 0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0 },
-	{ 0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0 },
-	{ 0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0 },
-	{ 0,0,0,0,0,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0 },
-	{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
-	{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
-	{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 } };
+	int matriz[20][23] = {
+		{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+		{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+		{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+		{ 0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0 },
+		{ 0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0 },
+		{ 0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0 },
+		{ 0,0,0,0,0,1,0,0,0,0,1,1,1,0,0,0,0,0,0,1,0,0,0 },
+		{ 0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0 },
+		{ 0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0 },
+		{ 0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+		{ 0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+		{ 0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+		{ 0,0,0,0,0,1,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0 },
+		{ 0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0 },
+		{ 0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0 },
+		{ 0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0 },
+		{ 0,0,0,0,0,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0 },
+		{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+		{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+		{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 } };
 
 	for (int indice1 = 0; indice1 < FILAS; indice1++)
 		for (int indice2 = 0; indice2 < COLUMNAS; indice2++)
@@ -158,5 +225,5 @@ int main()
 		delete[] mapa[indice];
 	delete[]mapa;
 	_getch();
-    return 0;
+	return 0;
 }
